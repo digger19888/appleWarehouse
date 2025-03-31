1086fc4f48ad026d478d75d27258f8f1aa7713dc
dkoreshkov <d.koreshkov88@gmail.com>
2025-03-31 11:17:54 +0300

    initial commit

---
+++ src/main/java/com/appleWarehouse/AppleWarehouse.java
@@ -0,0 +1,21 @@
+package com.appleWarehouse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppleWarehouse {
+    private List<Apple> apples = getAllApples();
+
+    private List<Apple> getAllApples() {
+        List<Apple> apples = new ArrayList<>();
+        apples.add(new Apple("red", 100));
+        apples.add(new Apple("red", 160));
+        apples.add(new Apple("red", 200));
+        apples.add(new Apple("green", 200));
+        apples.add(new Apple("green", 90));
+        apples.add(new Apple("green", 50));
+        apples.add(new Apple("yellow", 50));
+        apples.add(new Apple("yellow", 170));
+        return apples;
+    }
+}
